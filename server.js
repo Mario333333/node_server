65e6b331999a0a3e24633ef159dd0d1759d67266
Mario Cesar Gaytan Cruz <mariocgc@icloud.com>
2021-04-14 13:28:35 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const connection = require("express-myconnection");
+
+app.set("port", process.env.port || 9000);
+app.listen(app.get("port"), () => {
+  console.log("Runnig on port ", app.get("port"));
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my api");
+});
